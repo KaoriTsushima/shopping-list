45dd529f5acea4bbffd43483716753fed7c9e7d8
Kaori Tsushima <kaoritsushima0107@gmail.com>
2023-08-25 14:24:35 +0100

    changed code for matching dayand items

--- index.js
+++ index.js
@@ -4,32 +4,39 @@ const removeButton = document.getElementById("removeButton");
 const division = document.getElementById("division");
 
 addButton.addEventListener("click", function () {
-  const text = userInput.value;
+  const itemName = userInput.value;
+  const day = document.getElementById("weekday").value.toLowerCase();
 
-  // Check if the input is not empty
-  if (text) {
-    const divItem = document.createElement("p");
-    //  divItem.textContent = text;
-    const checkbox_label = document.createElement("label");
-    const checkbox = document.createElement("input");
-    checkbox.setAttribute("type", "checkbox");
-    const checkbox_id = `checkbox_${text}`;
-    checkbox.setAttribute("id", checkbox_id);
-
-    // creating label html element
-    divItem.appendChild(checkbox);
-    checkbox_label.setAttribute("for", checkbox_id);
-    checkbox_label.innerText = text;
-    divItem.appendChild(checkbox_label);
-
-    divItem.addEventListener("mouseout", function () {
-      divItem.style.backgroundColor = "";
-      divItem.style.fontWeight = "";
-    });
-
-    division.appendChild(divItem);
-    userInput.value = "";
+  if (!itemName) {
+    alert("Please enter an item.");
+    return;
   }
+  console.log(itemName, day);
+
+  const dayElement = document.getElementById(`${day}-items`);
+  //   dayElement.appendChild()
+
+  const divItem = document.createElement("p");
+  //  divItem.textContent = text;
+  const checkbox_label = document.createElement("label");
+  const checkbox = document.createElement("input");
+  checkbox.setAttribute("type", "checkbox");
+  const checkbox_id = `checkbox_${itemName}`;
+  checkbox.setAttribute("id", checkbox_id);
+
+  // creating label html element
+  divItem.appendChild(checkbox);
+  checkbox_label.setAttribute("for", checkbox_id);
+  checkbox_label.innerText = itemName;
+  divItem.appendChild(checkbox_label);
+
+  divItem.addEventListener("mouseout", function () {
+    divItem.style.backgroundColor = "";
+    divItem.style.fontWeight = "";
+  });
+
+  dayElement.appendChild(divItem);
+  userInput.value = "";
 });
 
 removeButton.addEventListener("click", function () {
